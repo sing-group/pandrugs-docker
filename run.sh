c58206cef7c6dd15ae16a96727e3d2b97ce309c8
lipido <dgpena@uvigo.es>
2018-03-23 17:43:10 +0100

    Ensures that /var/run/mysqld dir exists

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ ! -d $DATA_DIR/database ]]; then
-	echo "=> No database dir found on $DATA_DIR, creating..."
-	mkdir $DATA_DIR/database
+    echo "=> No database dir found on $DATA_DIR, creating..."
+    mkdir $DATA_DIR/database
 fi
 if [[ ! -d $DATA_DIR/database/mysql ]]; then
     echo "=> An empty or uninitialized MySQL data directory is detected in $DATA_DIR/database"
@@ -12,14 +12,16 @@ if [[ ! -d $DATA_DIR/database/mysql ]]; then
     #/create_mysql_admin_user.sh
     
     echo "=> Starting MySQL for initial setup..."
-	/usr/bin/mysqld_safe > /dev/null 2>&1 &
-	RET=1
-	while [[ RET -ne 0 ]]; do
-		echo "=> Waiting for confirmation of MySQL service startup"
-		sleep 5
-		mysql -uroot -e "status" > /dev/null 2>&1
-		RET=$?
-	done
+    mkdir -p /var/run/mysqld
+    chown mysql:mysql /var/run/mysqld
+    /usr/bin/mysqld_safe > /dev/null 2>&1 &
+    RET=1
+    while [[ RET -ne 0 ]]; do
+        echo "=> Waiting for confirmation of MySQL service startup"
+        sleep 5
+        mysql -uroot -e "status" > /dev/null 2>&1
+        RET=$?
+    done
 
     echo "=> Running /mysql-setup.sh"
     /mysql-setup.sh
